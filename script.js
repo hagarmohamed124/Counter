710bab69d7fdced5892f3ce93e7011de1bee2ad1
Hagar Mohamed <114417495+hagarmohamed124@users.noreply.github.com>
2023-07-30 02:56:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const counter = document.querySelector('#counter')
+const btns = document.querySelectorAll('.btn')
+
+let count = 0
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const styles = e.currentTarget.classList
+
+    if (styles.contains('increase')) {
+      count++
+    }
+    else if (styles.contains('decrease')) {
+        count--
+      }
+      else {
+         count = 0
+      }
+       
+      if(count > 0) {
+        counter.style.color ='green'
+        first.style.color ='green'
+      }
+
+    if(count < 0) {
+      counter.style.color ='red'
+      first.style.color ='red'
+    }
+    if(count === 0) {
+      counter.style.color ='black'
+      first.style.color ='black'
+    }
+   
+
+
+
+    counter.textContent = count
+  })
+})
+// countElemen.textContent = count
